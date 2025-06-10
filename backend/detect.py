ce7484df9573757326ea7e2d6f03ad20e435d66f
aditi <aditiprao1@gmail.com>
2025-06-10 07:33:19 +0530

    add detect.py

---
+++ backend/detect.py
@@ -0,0 +1,19 @@
+from ultralytics import YOLO
+from PIL import Image
+import torch
+import os
+
+# Load once
+model = YOLO("yolov11m.pt")
+
+def run_detection(image_path):
+    results = model.predict(image_path, conf=0.25, save=False)
+    bean_counts = {}
+
+    for result in results:
+        for box in result.boxes:
+            cls = int(box.cls[0])
+            name = model.names[cls]
+            bean_counts[name] = bean_counts.get(name, 0) + 1
+
+    return bean_counts
